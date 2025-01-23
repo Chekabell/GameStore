76170f378b71e72729db55201d49bfa861202530
Chekabell <nikita.toporischev@mail.ru>
2025-01-23 14:36:25 +0500

    Change shoppingcart and wishlist

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -41,5 +41,4 @@ urlpatterns = [
     path('api/wishlists/<int:id>/', WishListAPIView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-] + static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
